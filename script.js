f9db68ed8c87b7c0c3fd02f97fec3c97b278ef39
Chidubem Akpu <stanleychidubemakpu@gmail.com>
2022-07-01 06:56:24 +0100

    Made  better

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+"use strict"
+const forOpen = document.querySelector('#hamburger');
+const forClose = document.querySelector("#close-me");
+forOpen.addEventListener("click",openme);
+forClose.addEventListener("click",closeme);
 function openme(){
-    document.getElementById("menu-stuff").style.width="75vw";
+    document.getElementById('menu-stuff').style.width='75vw';
 }
+
 function closeme(){
-    document.getElementById("menu-stuff").style.width="0";
+    document.getElementById('menu-stuff').style.width='0';
 }
